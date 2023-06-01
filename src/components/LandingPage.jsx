96397529d37898d00de60d481996c80af65302e3
Josiah Morris <josiahbmorris@hotmail.com>
2023-06-02 04:15:46 +0900

    Created background linear-gradient elements for landing page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -3,6 +3,8 @@ const LandingPage = () => {
         <StyledLanding>
                 JOSIAH
                 MORRIS
+          <LandingPageGradient className='light'/>
+          <LandingPageGradient className='dark'/>
         </StyledLanding>
   )
 }
@@ -13,6 +15,18 @@ height: 100vh;
 width: 100vw;
 display: grid;
 z-index: 1;
+const LandingPageGradient = styled.div`
+height: 100%;
+width: 100%;
+transition-delay: 0.012s;
+&.dark {
+  position: absolute;
+  background: linear-gradient(#2A222A, #873582);
+  opacity: ${props => props.theme.label === 'dark' ? '1' : '0'};
+}
+&.light {
+  background: linear-gradient(#E4E7C1, #7FB9BD);
+}
 `
 
 export default LandingPage
